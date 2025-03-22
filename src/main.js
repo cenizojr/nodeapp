0c0a5e8e3db6c1e9a6f5c24d1f394bf7614c81a0
jme418 <jkingmues@gmail.com>
2025-03-22 21:46:35 +0100

    Change default server port from 8080 to 3000

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
